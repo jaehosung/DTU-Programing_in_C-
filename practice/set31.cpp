8ce44609c5af59bd900fb3d567917a74dcabbe28
Jaeho <jaehossung@gmail.com>
2016-05-15 01:26:22 +0200

    live code study 07

--- practice/set31.cpp
+++ practice/set31.cpp
@@ -3,18 +3,16 @@
 using namespace std;
 int main(){
     int n,m,l,i;// l : intervals; n :size;
-    int *input;
-    int *output;
     int max=0,interval;
     cin>>l>>n;
-    input = new int[n];
+    int input[n];
     for(i=0;i<n;i++){
         cin>>input[i];
         if(input[i]>max)
             max = input[i];
     }
     interval = ceil((double)max/l);
-    output = new int[l];
+    int output[l];
     for(i=0;i<l;i++){
         output[i]=0;
     }
